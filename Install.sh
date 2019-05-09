7a829626df538f8489380c1cf9cef216d6ba0e83
bakuniverse <30513244+bakuniverse@users.noreply.github.com>
2019-05-09 09:15:39 +0800

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -86,7 +86,7 @@ function Install()
   wget --no-check-certificate -qO "${AcceTmp}/bin/${AcceBin}" "https://github.com/MoeClub/lotServer/raw/master/${MyKernel}"
   [ ! -f "${AcceTmp}/bin/${AcceBin}" ] && Uninstall "Download Error! Not Found ${AcceBin}. "
   Welcome;
-  wget --no-check-certificate -qO "/tmp/lotServer.tar" "https://github.com/MoeClub/lotServer/raw/master/lotServer.tar"
+  wget --no-check-certificate -qO "/tmp/lotServer.tar" "https://github.com/bakuniverse/lotServer/raw/master/lotServer.tar"
   tar -xvf "/tmp/lotServer.tar" -C /tmp
   acce_ver=$(acce_check ${KNV})
   wget --no-check-certificate -qO "${AcceTmp}/etc/apx.lic" "https://api.moeclub.org/lotServer?ver=${acce_ver}&mac=${Mac}"
